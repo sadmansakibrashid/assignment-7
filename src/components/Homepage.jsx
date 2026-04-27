45e6101627adc5ad10ee43431f4928b62714a641
sadmansakibrashid <sadmansakibrashid@gmail.com>
2026-04-27 18:04:11 +0600

    fetching data from frinds.json

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,7 +1,10 @@
+import { use } from "react";
 import { FaPlus } from "react-icons/fa";
 
-
+const friendsPromise = fetch("/friends.json").
+then((res)=>res.json());
 const Homepage = () => {
+    const friends = use(friendsPromise);
     return (
          <div className="container mx-auto min-h-screen flex flex-col items-center text-center px-4">
       <h2 className="text-2xl font-semibold mb-4">
@@ -38,6 +41,35 @@ const Homepage = () => {
     <p className="text-gray-500">Interactions This Month</p>
   </div>
 </div>
+
+       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 mt-10">
+           
+            {
+                friends.map((friend)=>
+                {
+                    return (
+                    <div className="card bg-base-100 w-96 shadow-sm">
+  <figure className="px-10 pt-10">
+    <img
+      src={friend.picture}
+      className="rounded-xl" />
+  </figure>
+  <div className="card-body items-center text-center">
+    <h2 className="card-title">{friend.name}</h2>
+    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+    <div className="card-actions">
+      <button className="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
+                    );
+                    
+                }
+                )
+            }
+        </div> 
+    
+
 
     </div>
    
