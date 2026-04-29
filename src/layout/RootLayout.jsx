447407a3cef1aebeb7e908a273c240a76b2559af
sadmansakibrashid <sadmansakibrashid@gmail.com>
2026-04-29 18:55:16 +0600

    adding a loader and design footer according to figma

--- src/layout/RootLayout.jsx
+++ src/layout/RootLayout.jsx
@@ -2,12 +2,21 @@
 import { Outlet } from 'react-router';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
+import { Suspense } from 'react';
 
 const RootLayout = () => {
     return (
         <div>
             <Navbar></Navbar>
-            <Outlet></Outlet>
+            <Suspense
+        fallback={
+          <div className="flex justify-center items-center h-[60vh]">
+            <span className="loading loading-spinner loading-lg text-green-600"></span>
+          </div>
+        }
+      >
+        <Outlet />
+      </Suspense>
             <Footer></Footer>
         </div>
     );
